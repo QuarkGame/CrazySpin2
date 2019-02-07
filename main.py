979a46ac818f4f85eb5e3f91cb35170e075ebf3b
Michael <zwang20@knox.nsw.edu.au>
2019-02-07 14:10:36 +1100

    Init

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # initialisation
+
+# Imports
+import pygame
+import sys
+import os
+
+pygame.init()
+
+# colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+WIDTH = 800
+HEIGHT = 800
+display = pygame.display.set_mode((WIDTH, HEIGHT))
