bf3e83fab4aa311c20504064cefe8c8ed58c3235
Michael <zwang20@knox.nsw.edu.au>
2019-02-07 14:11:08 +1100

    game

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ BLUE = (0, 0, 255)
 WIDTH = 800
 HEIGHT = 800
 display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def game():
+    while True:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q] and (keys[pygame.K_LMETA] or keys[pygame.K_RMETA]) or pygame.event.peek(pygame.QUIT):  # Quit
+            pygame.quit()
+            sys.exit()
+        GameObj.family.draw(display)
+        pygame.display.update()
+
+game()
