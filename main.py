ad2c57f05faac107139fb8e2aa1c90e9501aa60e
Michael <zwang20@knox.nsw.edu.au>
2019-02-08 08:11:08 +1100

    Testing

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class GameObj(pygame.sprite.Sprite):
 
 
 class Ball(GameObj):
-    radius = 20
+    radius = 10
 
     family = pygame.sprite.RenderUpdates() # pygame.sprite.GroupSingle()
 
@@ -43,30 +43,23 @@ class Ball(GameObj):
         self.family.add(self)
 
 
-# class Block(GameObj):
-#
-#     # collision_detection
-#     family = pygame.sprite.Group()
-#
-#     # Constructor. Pass in the color of the block,
-#     # and its x and y position
-#     def __init__(self, color, width, height):
-#         super().__init__()
-#
-#         # Create an image of the block, and fill it with a color.
-#         # This could also be an image loaded from the disk.
-#         self.image = pygame.Surface([width, height])
-#         self.image.fill(color)
-#
-#         # Fetch the rectangle object that has the dimensions of the image
-#         # Update the position of this object by setting the values of rect.x and rect.y
-#         self.rect = self.image.get_rect()
-#         Block.family.add(self)
+class Pad(GameObj):
 
+    height = 10
+    width = 100
+
+    family = pygame.sprite.RenderUpdates()
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((self.height, self.width))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.family.add(self)
 
 def game():
     Ball()
-    # Block(GREEN, 100, 100)
+    Pad()
     while True:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_q] and (keys[pygame.K_LMETA] or keys[pygame.K_RMETA]) or pygame.event.peek(pygame.QUIT):  # Quit
