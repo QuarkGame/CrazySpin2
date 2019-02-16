cc3bff375249180cf2eb9e526127ae3547f2a152
Michael <zwang20@knox.nsw.edu.au>
2019-02-16 19:50:49 +1100

    New Colors

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import os
 pygame.init()
 
 # colours
-WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
+WHITE = (255, 255, 255); GREY = (128, 128, 128)
+RED = (255, 0, 0); DARK_READ = (128, 0, 0)
+GREEN = (0, 255, 0); DARK_GREEN = (0, 128, 0)
+BLUE = (0, 0, 255); DARK_BLUE = (0, 0, 128)
+CYAN = (0, 255, 255); DARK_CYAN = (0, 128, 128)
+MAGENTA = (255, 0, 255); DARK_MAGENTA = (128, 0, 128)
+YELLOW = (255, 255, 0); DARK_YELLOW = (128, 128, 0)
 
 WIDTH = 800
 HEIGHT = 800
